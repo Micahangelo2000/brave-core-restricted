dbe8838ce71b822fa1e45cb8898be1107611a788
Micahangelo2000 <109966427+Micahangelo2000@users.noreply.github.com>
2026-03-14 22:53:15 +0800

    Add SafeSearchNavigationThrottle class header

--- components/safe_search/safe_search_navigation_throttle.h
+++ components/safe_search/safe_search_navigation_throttle.h
@@ -1,1 +1,29 @@
+// safe_search_navigation_throttle.h
+#ifndef BRAVE_SAFE_SEARCH_SAFE_SEARCH_NAVIGATION_THROTTLE_H_
+#define BRAVE_SAFE_SEARCH_SAFE_SEARCH_NAVIGATION_THROTTLE_H_
 
+#include "content/public/browser/navigation_throttle.h"
+#include "url/gurl.h"
+
+namespace safe_search {
+
+class SafeSearchNavigationThrottle : public content::NavigationThrottle {
+ public:
+  explicit SafeSearchNavigationThrottle(content::NavigationHandle* handle);
+  ~SafeSearchNavigationThrottle() override;
+
+  // NavigationThrottle:
+  ThrottleCheckResult WillStartRequest() override;
+  ThrottleCheckResult WillRedirectRequest() override;
+
+  static std::unique_ptr<content::NavigationThrottle> MaybeCreateThrottleFor(
+      content::NavigationHandle* handle);
+
+ private:
+  bool HostIsSearchProvider(const GURL& url) const;
+  GURL MakeSafeUrl(const GURL& url) const;
+};
+
+}  // namespace safe_search
+
+#endif  // BRAVE_SAFE_SEARCH_SAFE_SEARCH_NAVIGATION_THROTTLE_H_
