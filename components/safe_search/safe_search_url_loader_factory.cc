750e4a4ccd2009a149c1b31176c832b355e70f04
Micahangelo2000 <109966427+Micahangelo2000@users.noreply.github.com>
2026-03-14 22:54:49 +0800

    Update safe_search_url_loader_factory.cc

--- components/safe_search/safe_search_url_loader_factory.cc
+++ components/safe_search/safe_search_url_loader_factory.cc
@@ -1,1 +1,19 @@
+// safe_search_url_loader_factory.h - conceptual
+class SafeSearchURLLoaderFactory : public network::mojom::URLLoaderFactory {
+ public:
+  SafeSearchURLLoaderFactory(mojo::PendingReceiver<network::mojom::URLLoaderFactory> receiver,
+                             network::mojom::URLLoaderFactory* inner_factory);
+  ~SafeSearchURLLoaderFactory() override;
 
+  // network::mojom::URLLoaderFactory:
+  void CreateLoaderAndStart(...) override {
+    // Inspect resource_request->url
+    // If host is a blocked-search engine or a search provider without safe param:
+    //   - modify url (append param) OR
+    //   - return a synthetic redirect response to the safe host (preferable)
+    // Otherwise delegate to inner_factory_->CreateLoaderAndStart(...)
+  }
+  void Clone(mojo::PendingReceiver<network::mojom::URLLoaderFactory> receiver) override;
+ private:
+  network::mojom::URLLoaderFactory* inner_factory_;
+};
